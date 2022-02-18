77dfc68140e0d3b81694458916a354ec6caa338e
orr <otavio.ramos@gmail.com>
2022-02-18 18:04:56 -0300

    cart mobile

--- functions.js
+++ functions.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector("#menu-div");
 const blackout = document.querySelector('#blackout');
 const navWrapper = document.querySelector('#nav-wrapper');
+const cartDiv = document.querySelector('#cart');
 menu.addEventListener('click', () => {
     
 
@@ -17,4 +18,9 @@ const close = document.querySelector("#close-icon");
 close.addEventListener('click', () => {
     navWrapper.classList.toggle('opened');
     blackout.classList.toggle('blackout');
+});
+
+cart = document.querySelector("#cart-button");
+cart.addEventListener('click', () =>{
+    cartDiv.classList.toggle('displayNone');
 });
